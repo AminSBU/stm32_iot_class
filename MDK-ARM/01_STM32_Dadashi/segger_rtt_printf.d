2519b9ad538e2c2a579430be218a34d335f3114e
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2026-01-26 08:54:27 +0330

    add RTT library

---
+++ MDK-ARM/01_STM32_Dadashi/segger_rtt_printf.d
@@ -0,0 +1,6 @@
+01_stm32_dadashi\segger_rtt_printf.o: ..\App\RTT-main\RTT\SEGGER_RTT_printf.c
+01_stm32_dadashi\segger_rtt_printf.o: ..\App\RTT-main\RTT\SEGGER_RTT.h
+01_stm32_dadashi\segger_rtt_printf.o: ..\App\RTT-main\Config\SEGGER_RTT_Conf.h
+01_stm32_dadashi\segger_rtt_printf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+01_stm32_dadashi\segger_rtt_printf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
+01_stm32_dadashi\segger_rtt_printf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
